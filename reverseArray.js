b4394bc4f76608087e615d24a0abb7a6d20c4b80
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-05-19 21:24:25 +0530

    ReverseArray : if array element contain string code added

--- reverseArray.js
+++ reverseArray.js
@@ -6,3 +6,25 @@ for (let start = 0, end = arr.length - 1; start < end; start++, end--) {
   arr[start] = arr[start] - arr[end]; // 7=8-1;
 }
 console.log(arr); //[7, 6, 5, 4,3, 2, 1];
+
+//ReverseArray in place? //if array element string
+
+function reverseArrayInPlace(array) {
+  let leftIndex = 0;
+  let rightIndex = array.length - 1;
+
+  while (leftIndex < rightIndex) {
+    // Swap elements at leftIndex and rightIndex
+    let temp = array[leftIndex];
+    array[leftIndex] = array[rightIndex];
+    array[rightIndex] = temp;
+
+    // Move indices towards the middle
+    leftIndex++;
+    rightIndex--;
+  }
+}
+
+let array = ["apple", "banana", "cherry", "date"];
+reverseArrayInPlace(array);
+console.log(array); // Output: ["date", "cherry", "banana", "apple"]
